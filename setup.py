16580ec33444933679ef9d029cd750a69e5d5ea4
Carlos M. Martinez <carlos@lacnic.net>
2017-05-22 15:50:48 -0300

    commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup 
 
 setup(name='webping.py',
-      version='0.1',
+      version='0.2',
       description='WebPing utility',
       author='Carlos Martinez',
       author_email='carlos@lacnic.net',
